6fe6173d9ae9b64a889af16f5e20b41bd982d180
Don1011 <dondaniel028@gmail.com>
2021-08-16 08:08:41 +0100

    USSD update

--- routes/vote.js
+++ routes/vote.js
@@ -5,26 +5,33 @@ const Voters = require('../models/Voters');
 const { CHECK_VOTER_ERROR } = require("../constants/errorMessages");
 
 router.post("/vote", async (req, res)=>{
+//     const { sessionId, serviceCode, phoneNumber, text } = req.body;
+//     const e = "END ";
+//     const c = "CON ";
+//     let response = "";
+//     phoneNumber = phoneNumber.replace(/+234/, "0");
+//     if(text === ""){
+//         // Check if the a user exists with the phone number.
+//         // Voters.find({phoneNumber})
+//         // .then(voter => {
+//         //     voter = voter[0];
+//             response = `${e}Voter's name: ${voter.firstname}`;
+//         // })
+//         // .catch(err => {
+//         //     response = CHECK_VOTER_ERROR
+//         // })
+//     }
+// 
+//     res.header("Content-type", "text/plain");
+//     res.end(response);
+//     
+
     const { sessionId, serviceCode, phoneNumber, text } = req.body;
-    const e = "END ";
-    const c = "CON ";
-    let response = "";
-    phoneNumber = phoneNumber.replace(/+234/, "0");
-    if(text === ""){
-        // Check if the a user exists with the phone number.
-        // Voters.find({phoneNumber})
-        // .then(voter => {
-        //     voter = voter[0];
-            response = `${e}Voter's name: ${voter.firstname}`;
-        // })
-        // .catch(err => {
-        //     response = CHECK_VOTER_ERROR
-        // })
-    }
+
+    let response = `END This is to verify that ${phoneNumber} actually entered a USSD code to access this service.`;
 
     res.header("Content-type", "text/plain");
     res.end(response);
-    
 })
 
 module.exports = router;
